cbd18f6ea3f3d76a0dcd8776e8dc04136ed63100
Jihee <sjh709@naver.com>
2025-07-06 18:30:09 +0900

    컴포넌트, 모듈

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-const API_URL = 'https://animal-api-two.vercel.app/';
+import App from './App';
 
-const $content = document.querySelector('div.content');
-let template = '';
+const $app = document.getElementById('app');
 
-const getData = async () => {
-  let res = await fetch(API_URL);
-  try {
-    if (res) {
-      let data = await res.json();
-      data.photos.forEach((elm) => {
-        template += `<img src=${elm.url}></img>`;
-      });
-      $content.innerHTML = template;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
+// let app = new App();
+// app();
 
-getData();
+new App($app);
