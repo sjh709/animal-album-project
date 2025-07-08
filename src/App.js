e73ce132bebba693169b6a648980e4e6a3ce469b
Jihee <sjh709@naver.com>
2025-07-08 18:07:53 +0900

    동물 앨범 만들기-2-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import TabBar from './components/TabBar';
-import Content from './components/Content';
+import TabBar from './components/TabBar.js';
+import Content from './components/Content.js';
+import { request } from './components/api.js';
 
 export default function App($app) {
-  const tabBar = new TabBar();
+  this.state = {
+    currentTab: 'all',
+    photos: [],
+  };
+  const tabBar = new TabBar({
+    $app,
+    initialState: '',
+    onClick: async (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
   const content = new Content();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  init();
 }
